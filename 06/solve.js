e5fb5ad401f09a7ea6a388030297549f5c982e1f
Alexander Schulze <github@lxhome.de>
2019-12-08 16:30:02 +0100

    day 06

--- 06/solve.js
+++ 06/solve.js
@@ -1,13 +1,30 @@
 adventofcode.activate(6);
 
 adventofcode.day6_part1 = function(input) {
-    let tree = adventofcode.day6_get_orbits(input);
+    adventofcode.day6_get_orbits(input);
 
     return adventofcode.day6_orbit_count;
 };
 
 adventofcode.day6_part2 = function(input) {
+    adventofcode.day6_get_orbits(input);
 
+    let orbital_jumps = 0;
+
+    // compare both rootlines and count every body,
+    // that's only present in either one of them
+
+    adventofcode.day6_orbit_objects_YOU.forEach(body => {
+        if (adventofcode.day6_orbit_objects_SAN.indexOf(body) < 0)
+            orbital_jumps++;
+    });
+
+    adventofcode.day6_orbit_objects_SAN.forEach(body => {
+        if (adventofcode.day6_orbit_objects_YOU.indexOf(body) < 0)
+            orbital_jumps++;
+    });
+
+    return orbital_jumps;
 };
 
 adventofcode.day6_get_orbits = function(input) {
@@ -16,33 +33,48 @@ adventofcode.day6_get_orbits = function(input) {
     const orbits = input.split("\n").forEach(x => {
         [inner, outer] = x.split(")");
 
-        if (typeof tree[inner] == 'undefined') {
+        if (typeof tree[inner] == 'undefined')
             tree[inner] = [];
-        }
+
         tree[inner].push(outer);
     });
 
     adventofcode.day6_orbit_count = 0;
 
-    tree = adventofcode.day6_build_tree(tree, 'COM', 0);
+    // rootlines
+    adventofcode.day6_orbit_objects_YOU = '';
+    adventofcode.day6_orbit_objects_SAN = '';
 
-    return tree['COM'];
+    adventofcode.day6_recursive_build_tree(tree, 'COM', 0, 'COM');
 };
 
-adventofcode.day6_build_tree = function(tree, body, orbit_count) {
+/**
+ * transform flat array into the tree of bodies
+ * @param {string[]} tree flat array of each body orbited by another body
+ * @param {string} body current body to process
+ * @param {int} orbit_count count of orbits (for checksum of part 1)
+ * @param {string} orbit_objects pipe-concatenated rootline of each body (for part 2)
+ */
+adventofcode.day6_recursive_build_tree = function(tree, body, orbit_count, orbit_objects) {
     adventofcode.day6_orbit_count += orbit_count;
 
+    let newtree = {};
+
     if (typeof tree[body] == 'undefined') {
-        return tree;
+        return newtree;
     }
 
-    tree[body].forEach((b, i) => {
-        delete tree[body][i];
+    tree[body].forEach(b => {
+        if (typeof newtree[b] == 'undefined')
+            newtree[b] = {};
 
-        tree[body][b] = tree[b];
+        if (b === 'SAN')
+            adventofcode.day6_orbit_objects_SAN = orbit_objects.split('|');
+        else if (b === 'YOU')
+            adventofcode.day6_orbit_objects_YOU = orbit_objects.split('|');
 
-        tree = adventofcode.day6_build_tree(tree, b, orbit_count+1);
+        newtree[b] = adventofcode.day6_build_tree(tree, b, orbit_count+1, b+'|'+orbit_objects);
     });
 
-    return tree;
+    return newtree;
 };
