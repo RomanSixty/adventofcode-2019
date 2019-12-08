3e342e7a3698b97d750a8fcacd85ba98b8cf33d7
Alexander Schulze <github@lxhome.de>
2019-12-08 21:45:54 +0100

    pulled opcode processor to library

--- 05/solve.js
+++ 05/solve.js
@@ -1,80 +1,9 @@
 adventofcode.activate(5);
 
 adventofcode.day5_part1 = function(input) {
-    return adventofcode.day5_opcode_process(input, 1);
+    return adventofcode.opcode_process(input, [1]);
 };
 
 adventofcode.day5_part2 = function(input) {
-    return adventofcode.day5_opcode_process(input, 5);
-};
-
-adventofcode.day5_opcode_process = function(input, input_suggestion) {
-    let program = input.split(",").map(x => parseInt(x));
-
-    let pointer = 0;
-    let output = '';
-
-    while (program[pointer] !== 99) {
-        const opcode   = program[pointer].toString().padStart(5, "0");
-        const op_parts = opcode.split("").map(x => parseInt(x));
-
-        // ATTENTION parameter order is reversed
-        // first:  op_parts[2]
-        // second: op_parts[1]
-        // third:  op_parts[0]
-
-        switch (op_parts[4]) {
-            case 1:
-                program[program[pointer+3]] = adventofcode.day5_get_parameter(program, pointer+1, op_parts[2]) + adventofcode.day5_get_parameter(program, pointer+2, op_parts[1]);
-                pointer += 4;
-                break;
-
-            case 2:
-                program[program[pointer+3]] = adventofcode.day5_get_parameter(program, pointer+1, op_parts[2]) * adventofcode.day5_get_parameter(program, pointer+2, op_parts[1]);
-                pointer += 4;
-                break;
-
-            case 3:
-                program[program[pointer+1]] = parseInt(prompt('Input: ', input_suggestion));
-                pointer += 2;
-                break;
-
-            case 4:
-                output = adventofcode.day5_get_parameter(program, pointer+1, op_parts[2]);
-                pointer += 2;
-                break;
-
-            // part 2
-            case 5:
-                if (adventofcode.day5_get_parameter(program, pointer+1, op_parts[2]) > 0) {
-                    pointer = adventofcode.day5_get_parameter(program, pointer+2, op_parts[1]);
-                } else {
-                    pointer += 3;
-                }
-                break;
-
-            case 6:
-                if (adventofcode.day5_get_parameter(program, pointer+1, op_parts[2]) === 0) {
-                    pointer = adventofcode.day5_get_parameter(program, pointer+2, op_parts[1]);
-                } else {
-                    pointer += 3;
-                }
-                break;
-
-            case 7:
-                program[program[pointer+3]] = (adventofcode.day5_get_parameter(program, pointer+1, op_parts[2]) < adventofcode.day5_get_parameter(program, pointer+2, op_parts[1])) ? 1 : 0;
-                pointer += 4;
-                break;
-
-            case 8:
-                program[program[pointer+3]] = (adventofcode.day5_get_parameter(program, pointer+1, op_parts[2]) === adventofcode.day5_get_parameter(program, pointer+2, op_parts[1])) ? 1 : 0;
-                pointer += 4;
-                break;
-        }
-    }
-    return output;
-};
-
-adventofcode.day5_get_parameter = function(program, pointer, mode) {
-    return (mode === 1) ? program[pointer] : program[program[pointer]];
+    return adventofcode.opcode_process(input, [5]);
 };
--- lib.js
+++ lib.js
@@ -30,3 +30,100 @@ let adventofcode = {
 };
 
 adventofcode.init();
+
+/**
+ * opcode processor
+ * used on days 5 and 7
+ *
+ * @param {string} input program source code
+ * @param {int[]} user_input
+ * @param {int} keep program in memory for another run
+ * @returns {string} return value
+ */
+adventofcode.opcode_process = function(input, user_input, keep = -1) {
+    let program = '';
+
+    program = input.split(",").map(x => parseInt(x));
+
+    if (keep >= 0 ) {
+        if (typeof adventofcode.opcode_memory == 'undefined')
+            adventofcode.opcode_reset();
+
+        if (typeof adventofcode.opcode_memory[keep] != 'undefined')
+            program = adventofcode.opcode_memory[keep];
+    }
+
+    let pointer = 0;
+    let output = '';
+
+    while (program[pointer] !== 99) {
+        const opcode   = program[pointer].toString().padStart(5, "0");
+        const op_parts = opcode.split("").map(x => parseInt(x));
+
+        // ATTENTION parameter order is reversed
+        // first:  op_parts[2]
+        // second: op_parts[1]
+        // third:  op_parts[0]
+
+        switch (op_parts[4]) {
+            case 1:
+                program[program[pointer+3]] = adventofcode.opcode_get_parameter(program, pointer+1, op_parts[2]) + adventofcode.opcode_get_parameter(program, pointer+2, op_parts[1]);
+                pointer += 4;
+                break;
+
+            case 2:
+                program[program[pointer+3]] = adventofcode.opcode_get_parameter(program, pointer+1, op_parts[2]) * adventofcode.opcode_get_parameter(program, pointer+2, op_parts[1]);
+                pointer += 4;
+                break;
+
+            case 3:
+                program[program[pointer+1]] = user_input.shift();
+                pointer += 2;
+                break;
+
+            case 4:
+                output = adventofcode.opcode_get_parameter(program, pointer+1, op_parts[2]);
+                pointer += 2;
+                break;
+
+            case 5:
+                if (adventofcode.opcode_get_parameter(program, pointer+1, op_parts[2]) > 0) {
+                    pointer = adventofcode.opcode_get_parameter(program, pointer+2, op_parts[1]);
+                } else {
+                    pointer += 3;
+                }
+                break;
+
+            case 6:
+                if (adventofcode.opcode_get_parameter(program, pointer+1, op_parts[2]) === 0) {
+                    pointer = adventofcode.opcode_get_parameter(program, pointer+2, op_parts[1]);
+                } else {
+                    pointer += 3;
+                }
+                break;
+
+            case 7:
+                program[program[pointer+3]] = (adventofcode.opcode_get_parameter(program, pointer+1, op_parts[2]) < adventofcode.opcode_get_parameter(program, pointer+2, op_parts[1])) ? 1 : 0;
+                pointer += 4;
+                break;
+
+            case 8:
+                program[program[pointer+3]] = (adventofcode.opcode_get_parameter(program, pointer+1, op_parts[2]) === adventofcode.opcode_get_parameter(program, pointer+2, op_parts[1])) ? 1 : 0;
+                pointer += 4;
+                break;
+        }
+    }
+
+    if (keep >= 0 )
+        adventofcode.opcode_memory[keep] = program;
+
+    return output;
+};
+
+adventofcode.opcode_get_parameter = function(program, pointer, mode) {
+    return (mode === 1) ? program[pointer] : program[program[pointer]];
+};
+
+adventofcode.opcode_reset = function() {
+    adventofcode.opcode_memory = [];
+};
