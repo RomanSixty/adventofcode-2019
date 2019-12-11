846d52996b79fc8bddf299e1400915b32f83e79f
Alexander Schulze <github@lxhome.de>
2019-12-12 00:49:19 +0100

    days 09 and 11

---
+++ 09/solve.js
@@ -0,0 +1,13 @@
+adventofcode.activate(9);
+
+adventofcode.day9_part1 = function(input) {
+    adventofcode.showImage('https://i.giphy.com/media/RddAJiGxTPQFa/giphy.webp');
+
+    return "please no more intcode tasks...";
+};
+
+adventofcode.day9_part2 = function(input) {
+    adventofcode.showImage('https://i.giphy.com/media/wYyTHMm50f4Dm/giphy.webp');
+
+    return "I mean it...";
+};
---
+++ 11/solve.js
@@ -0,0 +1,13 @@
+adventofcode.activate(11);
+
+adventofcode.day11_part1 = function(input) {
+    adventofcode.showImage('https://i.giphy.com/media/1zSz5MVw4zKg0/giphy.webp');
+
+    return "didn't I make myself clear about intcode tasks last time?";
+};
+
+adventofcode.day11_part2 = function(input) {
+    adventofcode.showImage('https://i.giphy.com/media/d10dMmzqCYqQ0/giphy.webp');
+
+    return "plz stahp!";
+};
