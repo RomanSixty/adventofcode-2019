66bce3579ce74e7c55eb60f7805589cd4458be1d
Alexander Schulze <github@lxhome.de>
2019-12-14 22:33:51 +0100

    day 13

---
+++ 13/solve.js
@@ -0,0 +1,13 @@
+adventofcode.activate(13);
+
+adventofcode.day13_part1 = function(input) {
+    adventofcode.showImage('https://i.giphy.com/media/LZ4YlxtpHWoIXMFLFC/giphy.webp');
+
+    return "Did I read intcode...";
+};
+
+adventofcode.day13_part2 = function(input) {
+    adventofcode.showImage('https://i.giphy.com/media/9J7i4fKvfqnRvpyNjM/giphy.webp');
+
+    return "*yawn*";
+};
