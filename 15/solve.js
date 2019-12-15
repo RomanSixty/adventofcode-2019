e3f4f6cb7a4ef5bad06424a0a1521101e0a9cc2b
Alexander Schulze <github@lxhome.de>
2019-12-15 15:10:43 +0100

    day 15

---
+++ 15/solve.js
@@ -0,0 +1,13 @@
+adventofcode.activate(15);
+
+adventofcode.day15_part1 = function(input) {
+    adventofcode.showImage('https://i.giphy.com/media/3o6ZsWAlFqQOHBNFLi/giphy.webp');
+
+    return "intcode again...";
+};
+
+adventofcode.day15_part2 = function(input) {
+    adventofcode.showImage('https://media.giphy.com/media/QORM9QVJYDM8wqd5vP/giphy.gif');
+
+    return "I'm slowly running out of reaction images";
+};
